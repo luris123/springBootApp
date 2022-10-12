d20559dcdd851a1ec3c8c5ef92d0cb99639656c5
luris123 <anssi.rauhala1@gmail.com>
2022-10-12 19:19:27 +0300

    first commit

---
+++ demo/src/main/java/com/courses/demo/CourseController/CourseController.java
@@ -0,0 +1,30 @@
+package com.courses.demo.CourseController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.courses.demo.Data.Course;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/courses") 
+    public List<Course> getCourses() {
+        return List.of(new Course("Java", "Spring Boot"),
+                new Course("Python", "Django"));
+    }
+
+    @PostMapping("addcourse")
+    public Course addCourse(@RequestBody Course course) {
+        return course;
+    }
+    
+}
